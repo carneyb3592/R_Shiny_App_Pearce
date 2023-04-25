f80303aaecc2d9ff145588e8dbb507ac82b25445
Michael Pearce <pearce790@gmail.com>
2023-04-25 14:00:10 -0700

    text and logo updates

--- Pearce_Shiny_App/ui/dataUI.R
+++ Pearce_Shiny_App/ui/dataUI.R
@@ -3,35 +3,35 @@ dataUI <- tabPanel("Data",
                    tabsetPanel(
                        tabPanel("Upload",
                                 br(),
-                                p("On this page, you may select a built-in toy dataset or 
-                                  upload your own. For specific instructions on the data 
-                                  formats required for rankings and ratings, see the instructions below."),
-                                
-                                h2("Example Datasets"),
-                                p("Choose one of the following..."),
+                                p("On this page, we select, explore, and visualize rankings and ratings data. Get started by either selecting
+                                either a 'toy dataset' or uploading your own data (instructions below)."),
+
+                                h2("Toy Datasets"),
+                                p("The following three toy datasets were described and analyzed in Gallo et al. (2023). Please select from the
+                                following options."),
                                 selectInput(
                                   "toyfile",
-                                  "Example file:",
-                                  choices = c("ToyData","ToyData2")
+                                  "Choose Toy Dataset:",
+                                  choices = c("ToyData1","ToyData2","ToyData3")
                                 ),
                                 tags$hr(),
                                 h2("Upload Your Own Data"),
+                                br(),
+                                p("Instead of using toy datasets, you may upload your own rankings and ratings for analysis. Each data type must be
+                                  uploaded separately using the instructions below."),
                                 h3("Rankings"),
-                                
-                                p("Rankings should be uploaded as a .csv file. If uploading ",em("Orderings,"),"the file 
-                                  should contain one row per reviewer. Each row will contain the reviewer's 
-                                  ordering of proposals from best to worst, with one proposal per column. 
-                                  If uploading ",em("Ranks,"),"the file should contain one row per reviewer and one 
-                                  column per proposal, such that the (i,j) entry contains the rank assigned 
-                                  by reviewer i to proposal j. NA indicates that proposal j was not accessible to 
-                                  reviewer i, while 0 indicates that a proposal was considered but not ranked."),
-                                
-                                fileInput(inputId = "RankingsFile", "Choose Rankings file",
+
+                                p("Rankings should be uploaded as a .csv file, in the form of a two-dimensional array with one row per reviewer and one column per proposal.
+                                If uploading ",em("Orderings,"),"each row should contain the corresponding reviewer's ordering of proposals from best to worst. If the
+                                reviewer only provided a partial ordering, values of NA should be used to complete the row. If uploading ",em("Ranks,")," each the (i,j)
+                                entry should contain the rank assigned by reviewer i to proposal j, where NA may be used to indicate that a proposal was not ranked."),
+
+                                fileInput(inputId = "RankingsFile", "Choose Rankings File:",
                                   accept = c(
                                      "text/csv",
                                      "text/comma-separated-values,text/plain",
                                      ".csv",
-                                     ".RData")        
+                                     ".RData")
                                 ),
                                 fluidRow(
                                     column(
@@ -45,7 +45,7 @@ dataUI <- tabPanel("Data",
                                           ),
                                         selected = "orderings"
                                         )
-     
+
                                       ),
                                     column(
                                       2,
@@ -63,15 +63,15 @@ dataUI <- tabPanel("Data",
 
                                 ),
                                 h3("Ratings"),
-                                p("Ratings should be uploaded as a .csv file. The file should contain one row per reviewer 
-                                  and one column per proposal, such that the (i,j) entry contains the rating assigned 
-                                  by reviewer i to proposal j. Ratings must be integer-valued, where ",strong("0 = best rating"),"
-                                  and ",strong("M = worst rating.")," The maximum score M can be inputted manually below."),
-                                fileInput("RatingsFile", "Choose Ratings File",
+
+                                p("Ratings should be uploaded as a .csv file, in the form of a two-dimensional array with one row per reviewer and one column per proposal.
+                                The (i,j) entry should contain the rating assigned by reviewer i to proposal j. Ratings must be integer-valued, where ",
+                                strong("0 = best rating")," and ",strong("M = worst rating.")," The maximum score M should be inputted manually below."),
+                                fileInput("RatingsFile", "Choose Ratings File:",
                                         accept = c(
                                           "text/csv",
                                           "text/comma-separated-values,text/plain",
-                                          ".csv")        
+                                          ".csv")
                                 ),
                                 fluidRow(
                                   column(
@@ -81,7 +81,7 @@ dataUI <- tabPanel("Data",
                                       label = "Maximum score (M)",
                                       value=4
                                     )
-                                    
+
                                   ),
                                   column(
                                     2,
@@ -96,66 +96,66 @@ dataUI <- tabPanel("Data",
                                       selected = "comma"
                                     )
                                   )
-                                  
+
                                 ),
                                 uiOutput("check_data"),
                                 actionButton("upload","Upload Data", width = "150px", height= "100px", style="color: white; background-color: green; float: right"),
                                 uiOutput("unloadButton")
                               ),
                      tabPanel("Summary",
+                              p("Below, we view the provided rankings and ratings in tabular form. Please explore the data below and confirm correct data entry."),
                               h2("Rankings"),
                               tags$hr(),
                               DT::dataTableOutput("dataTableRank"),
                               h2("Ratings"),
-                              tags$hr(), 
+                              tags$hr(),
                               DT::dataTableOutput("dataTableRate")
                             ),
                      tabPanel("EDA",
                               h2("Exploratory Data Analysis"),
-                              p("The following three plots visually summarize the rankings and ratings independently, as well as calculate the internal consistency of each reviewer."),
+                              p("We provide three plots to visually summarize rankings and ratings. You may hover over the data in each plot to view additional information."),
                               sidebarLayout(position = "left",
                                 sidebarPanel(
-                                  h3("Ratings Figure"),
-                                  p("The first plot displays boxplots of ratings assigned 
-                                    to each proposal, overlaid with the original data. The 
-                                    x-axis displays proposals in numerical order and the y-axis 
-                                    displays ratings from best (0) to worst ( M = ",textOutput("RankingsText",inline=TRUE),").",sep="")
-                                 
+                                  h3("Ratings by Proposal"),
+                                  p("The first plot displays side-by-side boxplots of ratings assigned to each proposal. The
+                                  x-axis displays proposals in numerical order and the y-asix displays ratings from best (0)
+                                    to worst ( M = ",textOutput("RankingsText",inline=TRUE),"). Hover over each boxplot for more information.",sep="")
                                 ),
                                 mainPanel(
                                   shinycssloaders::withSpinner(plotlyOutput("RatingsPlot")),
                                   downloadButton('downloadRatings','Download Plot')
-                                  
+
                                 )
                               ),
                               sidebarLayout(position = "left",
                                 sidebarPanel(
                                   h3("Rankings Figure"),
-                                  p("The second plot displays the stacked bar charts of ranks 
-                                    assigned to each proposal. The x-axis displays proposals 
-                                    in numerical order and the y-axis displays counts. 
-                                    Different rank levels are indicated using colors. 
+                                  p("The second plot displays stacked barcharts of the ranks
+                                    assigned to each proposal. The x-axis displays proposals
+                                    in numerical order and the y-axis displays counts.
+                                    Different rank levels are indicated using colors.
                                     Hover over each bar for more information.")
                                 ),
                                 mainPanel(
                                   shinycssloaders::withSpinner(plotlyOutput("RankingsPlot")),
                                   downloadButton('downloadRankings','Download Plot')
-                                  
+
                                 )
                               ),
                               sidebarLayout(position = "left",
                                 sidebarPanel(
                                   h3("Inconsistencies"),
-                                  p(" The third plot displays a histogram of reviewer-level inconsistencies. 
-                                    Here, inconsistency is defined as the total number of unique object pairs 
-                                    in which their order in the judge's ranking is opposite that in the judge's 
-                                    ratings. We do not count pairs in which the order of objects cannot be inferred
-                                    based on the ratings or rankings (e.g., due to missing data or rating ties).")
+                                  p(" The third plot displays a histogram of reviewer-level inconsistencies.
+                                    Here, inconsistency is defined as the total number of unique object pairs
+                                    for which their ordering in a judge's ranking does not match their implied ordering by
+                                    a judge's ratings. We do not count pairs in which the order of objects cannot be inferred
+                                    based on ratings or rankings (e.g., due to missing data or rating ties). A value of 0 indicates a judge
+                                    whose ratings were internally consistent with his/her ranking.")
                                 ),
                                 mainPanel(
                                   shinycssloaders::withSpinner(plotlyOutput("InconsistenciesPlot")),
                                   downloadButton('downloadInconsistencies','Download Plot')
-                                  
+
                                 )
                               )
                    )
--- Pearce_Shiny_App/ui/homeUI.R
+++ Pearce_Shiny_App/ui/homeUI.R
@@ -2,18 +2,18 @@ homeUI <- tabPanel("Home",
                    icon = icon("house"),
                    h1("Peer Review with Rankings and Ratings"),
                    p("Welcome to the ",em("Peer Review with Rankings and Ratings")," R Shiny app! This interactive application fits joint statistical models to peer review data in the form of rankings and ratings, as well as provides tools for data exploration and decision-making."),
-                   p("Get started with the tool by clicking on the ",em("Data")," tab in the toolbar above, which allows you to get started using either your own data or a built-in toy dataset."),
+                   p("Get started with the tool by clicking on the ",em("Data")," tab in the toolbar above, which allows you to use either your own data or a built-in toy dataset."),
                    h2("Further Information"),
                    p("Additional information on the models included in this tool can be found in the following papers:"),
                    tags$ul(
+                     tags$li('Gallo, Stephen A., et al. "A new approach to peer review assessments: Score, then rank." (2023).'),
                      tags$li('Pearce, Michael, and Elena A. Erosheva. "A unified statistical learning model for rankings and scores with application to grant panel review." ',em("Journal of Machine Learning Research"),'23.210 (2022): 1-33.'),
-                     tags$li('Pearce, Michael, and Elena A. Erosheva. "Modeling Preferences: A Bayesian Mixture of Finite Mixtures for Rankings and Ratings." ',em('arXiv preprint arXiv:2301.09755'),'(2023).'),
-                     tags$li('Gallo, Stephen A., et al. "A new approach to peer review assessments: Score, then rank." (2022).')
+                     tags$li('Pearce, Michael, and Elena A. Erosheva. "Modeling Preferences: A Bayesian Mixture of Finite Mixtures for Rankings and Ratings." ',em('arXiv preprint arXiv:2301.09755'),'(2023).')
                    ),
                    h2("Feedback"),
                    p('If you discover any issues when working with this tool, please write to mpp790@uw.edu or use', a(href="https://github.com/carneyb3592/R_Shiny_App_Pearce","Github.")),
                    h2("Funding"),
                    p("This project was supported by the National Science Foundation under Grant No. 2019901.")
-                   
-                   
+
+
 )
